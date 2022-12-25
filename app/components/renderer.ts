52c998de530ade5d6086e803f1a9911af20f5107
Jon Ribeiro <contact@jonathas.com>
2022-12-25 12:00:04 +0100

    refactor: refactored the renderer

--- app/components/renderer.ts
+++ app/components/renderer.ts
@@ -3,49 +3,57 @@ import Timer from './timer';
 import Data from '../helpers/data';
 
 class Renderer {
+  private linkAbout: Element;
+  private playButton: HTMLImageElement;
+  private time: Element;
+  private course: Element;
+
   public constructor() {
+    this.setSelectors();
     this.addEventListeners();
   }
 
-  private addEventListeners() {
-    const linkAbout = document.querySelector('#link-about');
-    const playButton = document.querySelector('.play-button') as HTMLImageElement;
-    const time = document.querySelector('.time') as Element;
-    const course = document.querySelector('.course') as Element;
+  private setSelectors() {
+    this.linkAbout = document.querySelector('#link-about') as Element;
+    this.playButton = document.querySelector('.play-button') as HTMLImageElement;
+    this.time = document.querySelector('.time') as Element;
+    this.course = document.querySelector('.course') as Element;
+  }
 
+  private addEventListeners() {
     window.onload = async () => {
-      const data = await Data.get(course?.textContent || '');
-      time.textContent = data?.time || '00:00:00';
+      const data = await Data.get(this.course?.textContent || '');
+      this.time.textContent = data?.time || '00:00:00';
     };
     
-    linkAbout?.addEventListener('click' , function(){
+    this.linkAbout?.addEventListener('click' , () => {
       ipcRenderer.send('open-about-window');
     });
     
     let imgs = ['play', 'stop'];
     let play = false;
-    playButton?.addEventListener('click', function(){
+    this.playButton?.addEventListener('click', () => {
       if (play) {
-        Timer.stop(course?.textContent || '');
+        Timer.stop(this.course?.textContent || '');
         play = false;
       } else {
-        Timer.start(time);
+        Timer.start(this.time);
         play = true;
       }
     
       imgs = imgs.reverse();
-      playButton.src = `../assets/img/${imgs[0]}-button.svg`;
+      this.playButton.src = `../assets/img/${imgs[0]}-button.svg`;
     });
 
     ipcRenderer.on('course-changed', async (_event, courseName) => {
-      await this.updateCourseData(course, time, courseName);
+      await this.updateCourseData(courseName);
     });
   }
 
-  private async updateCourseData(course: Element, time: Element, courseName: string) {
-    course.textContent = courseName;
+  private async updateCourseData(courseName: string) {
+    this.course.textContent = courseName;
     const courseData = await Data.get(courseName);
-    time.textContent = courseData?.time || '00:00:00';
+    this.time.textContent = courseData?.time || '00:00:00';
   }
 }
 
